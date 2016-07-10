3603f55129a95f9b9dc9f8932877d6bdbacf6d9f
Jiyeon Lee <a1919361@gmail.com>
2016-07-10 15:11:32 +0900

    파일이동

---
+++ 1606day01-11/day04/js/check-support.js
@@ -0,0 +1,3 @@
+//스크립트가 지원되는 환경이라면
+//<html>요소의 class 속성 값을 no-js 에서 js로 변경
+var html=document.
---
+++ 1606day01-11/day04/js/in-body-javascript.js
@@ -0,0 +1,2 @@
+document.write('현재 사용자의 환경은 온라인 입니까?', window.navigator.onLine ? '네 온라인입니다':'아뇨 오프라인이오');
+document.write('<h1>This is javascript</h1>');
