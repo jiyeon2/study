a8cdcd0154a1caa56a8bf12cef5729a249f8de63
Jiyeon Lee <a1919361@gmail.com>
2016-08-17 13:05:09 +0900

    [resource] 헬퍼함수 추가 및 오타수정

--- resource/DOMHelper/index.js
+++ resource/DOMHelper/index.js
@@ -79,40 +79,25 @@ var parent_node = target_node.parentNode;
 if( next_node ){
 	next_node.parentNode.insertBefore(insert_node,next_node);
 }
-else{
-parent_node.appendChild(insert_node);
+else{parent_node.appendChild(insert_node);}
 }
-}
-
-/*--------------------------------------
-query, queryAll 함수
-
------------------------------------------*/
-
-//문자열인지 아닌지 확인하는 유효성검사 필요
-// 	if (typeof selector !== 'string'){throw new Error('전달인자는 문자열만 가능'); }//에러 던지면 함수 멈춤, native js구문이라 예전 브라우저에서도 가능, 코드 멈추면 안되는 경우 제외하곤 이거 사용 권장
-// 	// if (typeof selector !== 'string'){console.error('오류'); }//오류발생하지만 코드는 실행됨, 신형 브라우저에서만 가능
 
 
 /*--------------------------------------
 .querySelectorAll() 메소드 단축하여 사용할 수 있는 헬퍼 함수
-@return {undefined}
 -----------------------------------------*/
 function queryAll(selector_str, context){
 	//사용자가 올바른 데이터를 전달하였는가? 검증
 	if(typeof selector_str !== 'string'){ 
 		//조건이 참이 되면 오류 발생
-		throw new Error('첫번째 전달인자는 문자 유형이어야 합니다.');
+		throw new Error('첫번째 전달인자는 문자 유형이어야 합니다.'); //함수 실행 멈춤
 	}
 	//context 인자 값을 사용자가 전달하였는가?
 	// 사용자가 context값을 전달하지 않았을 경우 undefined이다
-	if(!context){
-		context = document;
-	}
+	// if(typeof context === 'undefined'){
+	if(!context){ context = document; }
 	return context.querySelectorAll(selector_str);
 }
-// typeof null, typeof [] : typeof 오류
-// 변수 전달시 '' 필요없음, 문자열 전달시 ''로 감싼다
 
 function query(selector, parent){
 	return queryAll(selector, parent)[0];
@@ -133,13 +118,13 @@ $q(선택자, 조건, 컨텍스트)
 @param {ELEMENT_NODE}  context 목표 선택자가 존재하는 부모노드
 @return {undefined}		 hook == 1 인 경우 
 --------------------------------------------------------*/
-function $q(selector, hook, context){
-	if(hook == 1){
-		query(selector, context);
-	} else{
-	return queryAll(selector, context);
-	}
-}
+// function $q(selector, hook, context){
+// 	if(hook == 1){
+// 		query(selector, context);
+// 	} else{
+// 	return queryAll(selector, context);
+// 	}
+// }
 
 // return 하나만 쓰는 경우
 function $q(selector, hook, context){
@@ -209,11 +194,6 @@ function createNode(element_name, text){
 	return element_node;
 }
 
-// 요소에 클래스 붙이기
-// function addClass(element_node, class_name){
-// element_node.setAttribute('class', class_name);
-// }
-
 /*------------------------------------------------
 웹브라우저에서 계산된 CSS 스타일 값 가져오는 방법
 --------------------------------------------------
@@ -257,12 +237,18 @@ function getStyle(el, property, pseudo){
 /*--------------------------------------
 css서 사용하는 '-'(하이픈) 붙은 속성명을 
 camelCase형식으로 변경하는 함수
+전달된 텍스트를  카멜케이스화 하여 변환하는 헬퍼 함수
 
 camelCase('font-size') -> fontSize 반환
 -----------------------------------------*/
-function camelCase(css_prop){
-	return css_prop.replace(/-./g, function($1){
-		return $1.replace('-','').toUpperCase();
+// function camelCase(css_prop){
+// 	return css_prop.replace(/-./g, function($1){
+// 		return $1.replace('-','').toUpperCase(); //정규표현식
+// 	});
+// }
+function camelCase(text){
+	return text.replace(/(\s|-|_)./g, function($1){
+		return $1.replace(/(\s|-|_)./g,'').toUpperCase();
 	});
 }
 
@@ -273,17 +259,51 @@ function cl(content){
 	console.log(content);
 }
 
-
+/*--------------------------------------
+오류메시지 출력하는 헬퍼함수
+-----------------------------------------*/
 function errorMsg(message){
 if(isType(message) !== 'string'){
 	//함수 자신을 다시 호출: 재귀함수
-	errorMsg('ㅇ류메시지는 문자 데이터 유형이어야 합니다');
+	errorMsg('오류메시지는 문자 데이터 유형이어야 합니다');
 }
 throw new Error(message);
 }
 
 /*--------------------------------------
-prevEl
+요소노드인지 아닌지 체크하여 참/거짓 반환
+isElNode() 요소 전달시 true false로 반환
+-----------------------------------------*/
+function isElNode(node){
+return node.nodeType === 1;
+}
+function isntElNode(node){
+return !isElNode(node);
+//return node.nodeType !==1;
+}
+
+/*--------------------------------------
+요소노드 이름이 동일한지  체크하는 함수
+-----------------------------------------*/
+function isElName(node, name){
+if(isntElNode(node)){errorMsg('첫번째 인자로요소 노드가 전달되어야 합니다')}
+if(isType(name) !== 'string'){errorMsg('두번째 인자로요소 노드가 전달되어야 합니다')}
+return (node.localName || node.nodeName.toLowerCase()) === name;
+}
+
+/*--------------------------------------
+텍스트노드 유형인지 아닌지 체크하는 함수
+-----------------------------------------*/
+function isTextNode(node){
+	return node.nodeType === 3;
+}
+function isntTextNode(node){
+	//return !isTextNode(node);
+	return node.nodeType !== 3;
+}
+
+/*--------------------------------------
+이전 형제요소 노드 찾기
 -----------------------------------------*/
 function prevEl(node){
 	//검증: 유효성검사
@@ -291,27 +311,24 @@ function prevEl(node){
 		errorMsg('전달된 인자는 요소노드여야 합니다.');
 	}
 	// node.previousSibling; //요소노드, 텍스트노드, 주석노드 뭐가 존재하는지 모름
-
-
-	if(node.previousElementSibling){
+	if(node.previousElementSibling){// 구형 IE 9이상, 신형 웹 브라우저에서 사용
 		return node.previousElementSibling;
-	} 
-//구형 IE 6,7,8 지원
-	else{
-	do{
-		node = node.previousSibling; //앞에 노드가 존재하든 아니든 한번은 실행됨
-	}while(node && !isElNode(node)); //앞의 코드를 반복한느데 그 조건은 1. node가 존재 2. node는 요소노드가 아닌경우.
-	//만약 노드가 존재하지 않거나, 노드가 요소노드이면 반복문 종료하고 그 노드를 반환함
-	return node;
+	} else{ // 구형 IE 6~8에서 아래 코드 사용
+			do{ node = node.previousSibling }
+			while(node && !isElNode(node)); //앞의 코드를 반복한느데 그 조건은 1. node가 존재 2. node는 요소노드가 아닌경우.
+			//만약 노드가 존재하지 않거나, 노드가 요소노드이면 반복문 종료하고 그 노드를 반환함
+			return node;
 	}
 }
 
+/*--------------------------------------
+다음 형제요소 노드 찾기
+-----------------------------------------*/
 function nextEl(node){
 	//검증: 유효성검사
 	if(isntElNode(node)){
 		errorMsg('전달된 인자는 요소노드여야 합니다.');
 	}
-
 //구형 IE 9+, 신형 웹 브라우저. 함수 실행할 때 마다 조건안의 코드 지원하는 신형브라우저냐고 물어보는거랑 같아서 좋은 코드는 아님
 	if(node.nextElementSibling){
 		return node.nextElementSibling;
@@ -325,28 +342,13 @@ function nextEl(node){
 	}
 }
 
-/*--------------------------------------
-isElNode() 요소 전달시 true false로 반환
------------------------------------------*/
-function isElNode(node){
-return node.nodeType === 1;
-}
-function isntElNode(node){
-return !isElNode(node);
-}
-/*--------------------------------------
-요소노드 이름이 동일한지  체크하는 함수
------------------------------------------*/
-function isElName(node, name){
-if(isntElNode(node)){errorMsg('첫번째 인자로요소 노드가 전달되어야 합니다')}
-if(isType(name) !== 'string'){errorMsg('두번째 인자로요소 노드가 전달되어야 합니다')}
-return (node.localName || node.nodeName.toLowerCase()) === name;
-}
+
 
 /*--------------------------------------
 첫번째 자식요소 노드를 찾는 헬퍼 함수,
 마지막 자식요소 노드를 찾는 헬퍼 함수
 -----------------------------------------*/
+//목적에 도달하는 쉬운 헬퍼함수
 function _firstEl(node){
 	return node.children[0];
 }
@@ -357,10 +359,9 @@ function _lastEl(node){
 }
 
 function fistEl(node){
-
-	if(istnElNode(node)){errorMsg('요소노드를 전달해야 합니다');}
-	if(node.firtElementChild){
-		return node.firstElementChilde;
+	if(isntElNode(node)){errorMsg('요소노드를 전달해야 합니다');}
+	if(node.firstElementChild){
+		return node.firstElementChild;
 	}else{
 		// IE 6-8
 		// node 찾고자 하는 자식 노드의 부모이다
@@ -370,22 +371,47 @@ function fistEl(node){
 		//만약 찾은 자식 노드의 요소가 노드가 아니라면 다음 형제 노드를 찾는다. 
 		//  다음 형제 노드가 요소 노드라면 반환하고, 아니라면 다음 다시 형제 노드를 요소노드인지 확인한다
 		//console.log( node&&isntElNode(node) ? nextEl():node);
-		return (node && insntEl(none)) ? nextEl(node) : node;
+		return (node && isntElNode(node)) ? nextEl(node) : node;
 	}
 	// 함수는 명시적으로 어떤 값도 반환하지 않을 경우 undefined 반환
 	// return undefiled;	
 }
 
-/*--------------------------------------
-단위 제거 가져오기 소유하고 있는지 확인
------------------------------------------*/
-// function getUnit(value){
-// 	var i=0, l=getUnit.units.length, unit;
-// 	var reg;
-// 	for (; i<l; i++)
-// }
+function lastEl(node){
+	if (isntElNode(node)){errorMsg('요소노드를 전달해야 합니다');}
+	if (node.lastElementChild){
+		return node.lastElementchild;
+	}else{
+		node = node.lastChild;
+		return (node && isntElNode(node)) ? prevEl(node):node;
+	}
+}
 
-// function removeUnit(value){
-// 	removeUnit.unit = getUnit(value);
-// 	return parseFloat()
-// }
+/*--------------------------------------
+단위 제거/가져오기/소유하고 있는지 확인
+-----------------------------------------*/
+function getUnit(value){
+	var i=0, l = getUnit.units.length, unit;
+	var reg;
+	for (; i<1; i++){
+		unit = getUnit.units[i];
+		if (value.indexOf(unit)> -1){
+			//break;
+			return unit;
+		}
+	}
+	return null;
+}
+
+//함수 getUnit의 속성인 units에 배열 저장
+getUnit.units = 'px rem em % vw vh vmin vmax'.split(' '); // 문자열을 배열로
+
+function removeUnit(value){
+	removeUnit.unit = getUnit(value);
+	return parseFloat(value, 10);
+}
+removeUnit.unit = null;
+
+function hasUnit(value){
+	return !!getUnit(value);
+}
