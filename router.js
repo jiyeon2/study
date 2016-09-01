8b11ddc58e15da1126a453d24a864f78638bc1a6
Jiyeon Lee <a1919361@gmail.com>
2016-09-01 18:03:18 +0900

    install body-parser

--- router.js
+++ router.js
@@ -2,7 +2,9 @@ var fs = require("fs");
 var renderer = require("./renderer");
 
 function home(req, res){
-	if(req.url == "/"){	return renderer(req, res, "home", {});}
+	if(req.url == "/"){	
+		return renderer(req, res, "home", {});
+	}
 }
 
 //contact가 detail보다 밑에있으면 detail아래 붙을 수 있으므로 먼저 쓴다
