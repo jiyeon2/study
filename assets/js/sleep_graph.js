2c81b4d5c8c1b83fced98047732081182eb7f569
Jiyeon Lee <a1919361@gmail.com>
2016-09-11 01:43:22 +0900

    막대그래프 transition으로 애니메이션 적용

--- assets/js/sleep_graph.js
+++ assets/js/sleep_graph.js
@@ -1,19 +1,13 @@
-var sleepTimeData;
+
 var body=d3.select('body');
-var num = 40; // 수치보정값
+var num = 40; // 막대그래프 길이보정값
 
 d3.csv('../../../assets/data/july_sleep.csv', function(error, data){
-	sleepTimeData = data;
+
 	var w = 700;
 	var h = 500;
-	// var xScale = d3.scale.ordinal()
-	// 								.domain(d3.range(sleepTimeData.sleepTime.length))
-	// 								.rangeRoundBands([0,w], 0.05);
-	// var yScale = d3.scale.linear()
-	// 								.domain([0, d3.max(sleepTimeData.sleepTime)])
-	// 								.range([0,h]);
 
-	var svg = body.append('svg')
+	var svg = body.append('svg') //svg 태그 생성
 			.attr({
 				width : w,
 				height : h,
@@ -23,34 +17,43 @@ d3.csv('../../../assets/data/july_sleep.csv', function(error, data){
 				padding: '20px',
 				margin: '20px',
 				border: '1px solid blue'
-			})
-		.append('g');
-		// console.log(svg);
-	var bar_group = svg.append('g')
-		.attr('class','bar');
-	// console.log(bar_group);
-	bar_group.selectAll('rect')
-			.data(sleepTimeData)
-		.enter().append('rect')
-			.attr({
-				x : function(d,i){ return i*20; }, // return xScale(i)
-				y : function(d){return 500 - d.sleepTime*num;},// return h-yScale(d)
-				width : 14, //xScale.rangeBand()
-				height : function(d,i){return d.sleepTime*num;}, // fundtion (d,i){ return yScale(d);}
-				fill: '#89dc75'
-			})
-			.on('mouseover', function(d){
+			});
+
+	var bar_group = svg.append('g') //svg태그 자식으로 <g class="bars">생성
+		.attr('class','bars');
+
+	bar_group.selectAll('rect') //rect요소 지정
+			.data(data)							//data를 요소에 연결
+			.enter()								//각 데이터가 들어갈 객체 생성
+			.append('rect')					//객체와 연결된 rect요소들을 생성
+			.attr({									//각각의 rect에 속성할당
+				fill: '#89dc75',
+				x: function(d,i){ return i*20; } , 
+				// d: data, i: index / 각 rect x좌표값을 index*20으로 설정 -> 20px씩 떨어져있게됨
+				y: 500, // 초기 y값을 맨 아래에 둠
+				width: 14,
+				height: 0
+				}) 
+			.transition() //애니메이션 추가. 애니메이션 지정할 속성을 .transition()을 아래에 쓴다
+			.attr('y', function(d,i){return 500 - d.sleepTime*num; })
+			.attr('height',function(d,i){return d.sleepTime*num;})
+			.duration(1000) //1000ms 후 시작
+			.delay(function(d,i){return i*50}); // 각 rect마다 지연시간을 줌
+
+
+//마우스 올라갔을 때 막대그래프 위에 툴팁 띄우는 이벤트 설정
+	bar_group.selectAll('rect') //모든 막대그래프 선택
+			.on('mouseover', function(d){ //마우스 올라갔을 때 이벤트 설정
 				d3.select(this)
-					.attr('fill','#89ffad');
+					.transition()
+					.attr('fill','#45ac67');
 
-				var xPos = parseFloat(d3.select(this).attr('x')) - 30; 
-				//+ xScale.rangeBand() /2;
-				var yPos = parseFloat(d3.select(this).attr('y'))/ 2 + h/7;
-				var time_minute = (d.sleepTime+'').split('.'),
-						time        = time_minute[0],
-						minute      = time_minute[1];
-
-
+				var xPos = parseFloat(d3.select(this).attr('x')) - 30; //툴팁의 x좌료
+				
+				var yPos = parseFloat(d3.select(this).attr('y'))/ 2 + h/7; //툴팁의 y좌표
+				var time_minute = (d.sleepTime+'').split('.'), 
+						time        = time_minute[0], //수면 시간
+						minute      = time_minute[1]; //수면 분
 
 				d3.select('.tooltip')
 					.style({
@@ -61,12 +64,11 @@ d3.csv('../../../assets/data/july_sleep.csv', function(error, data){
 					.text(time+'시간 '+minute+'분');
 				d3.select('.tooltip').classed('hidden',false);
 			})
-			.on('mouseout', function(){
+			.on('mouseout', function(){ //마우스가 나갔을 때 이벤트 설정
 				d3.select(this)
+					.transition()
 					.attr('fill','#89dc75');
 				d3.select('.tooltip').classed('hidden',true);
 			})
-
-
 
 });
