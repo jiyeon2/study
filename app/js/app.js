394157e6d0b8afc8d056fbca820c7d97a1b952e1
Jiyeon Lee <a1919361@gmail.com>
2016-09-20 21:13:16 +0900

    url 수정

--- app/js/app.js
+++ app/js/app.js
@@ -39,7 +39,7 @@
 
 	 	$http({
 	 		'method':'GET',
-	 		'url':'//api.randomuser.me/?results=10',
+	 		'url':'https://api.randomuser.me/?results=10',
 	 		'headers': "{'Content-Type': 'application/json; charset=utf-8'}"
 	 	}).then(successAjaxCall, errorAjaxCall);
 
