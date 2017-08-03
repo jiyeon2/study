8b1a5d8bb3143c1104a876a0e1c0f61b1942426f
jiyeon lee <a1919361@gmail.com>
2017-08-03 16:41:35 +0900

    solve 2-1

--- jiyeon/02.divideAndQunquer/01.java
+++ jiyeon/02.divideAndQunquer/01.java
@@ -1,27 +1,39 @@
 import java.util.*;
 
 class Main{
+    public static int search(int[] arr, int target, int first, int last){
+        int mid;
+        if (first > last) {
+            return 0;
+        } else {
+            mid = (first+last)/2;
+            if ( target == arr[mid]){
+                return 1;
+            } else if( target < arr[mid]){
+                return search(arr, target, first, mid-1);
+            } else {
+                return search(arr, target, mid+1,last);
+            }
+        }
+
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int myCardCount = sc.nextInt();
-        ArrayList<Integer> myCards = new ArrayList<Integer>();
+        int[] myCards = new int[myCardCount];
 
         for(int i =0; i < myCardCount; i++){
-            myCards.add(sc.nextInt());
+            myCards[i] = sc.nextInt();
+        }
+        Arrays.sort(myCards);
+
+        int otherCardCount = sc.nextInt();
+        for(int i =0; i<otherCardCount; i++){
+            int thisCard = sc.nextInt();
+            System.out.print(search(myCards, thisCard, 0, myCards.length -1)+ " ");
         }
 
-        int checkCardCount = sc.nextInt();
-        int[] resultList = new int[checkCardCount];
-        for(int i=0; i < checkCardCount; i++){
-            int checkNum = sc.nextInt();
-            resultList[i] = myCards.contains(checkNum) ? 1 : 0;
-        }
-
-        String result = "";
-        for (int num : resultList){
-            result = result + num + " ";
-        }
-        System.out.println(result);
     }
 }
