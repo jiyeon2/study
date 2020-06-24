acdc53b11094a555b4ba84e2ad690e94fd741057
jiyeon lee <a1919361@gmail.com>
2020-06-24 14:08:22 +1200

    try to make custom video controller - progress bar, skip, stop, play button

---
+++ 11 custom video player/script.js
@@ -0,0 +1,34 @@
+// https://developer.mozilla.org/en-US/docs/Web/Guide/Audio_and_video_delivery/Video_player_styling_basics
+
+const video = document.querySelector('video');
+const progressBar = document.querySelector('#progress');
+const playButton = document.querySelector('#play');
+const stopButton = document.querySelector('#stop');
+const skipforwardButton = document.querySelector('#skip-forward');
+const skipBackwardButton = document.querySelector('#skip-backward');
+
+// console.log(video.duration);
+// progressBar.max = +video.duration;
+// progressBar.max NaN 뜸..
+// https://stackoverflow.com/questions/40763057/trying-to-get-full-video-duration-but-returning-as-nan
+
+// HTMLMediaElement에서 상속받은 메서드 이용
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+function playVideo(){
+    video.play();
+}
+function stopVideo(){
+    video.pause();
+}
+function skip(second){
+    video.currentTime = video.currentTime + second;
+}
+video.addEventListener('timeupdate',(e) => {progressBar.value = e.target.currentTime;})
+video.addEventListener('loadedmetadata', () => {progressBar.max = video.duration});
+playButton.addEventListener('click', playVideo);
+stopButton.addEventListener('click', stopVideo);
+skipBackwardButton.addEventListener('click', () => {skip(-1)});
+skipforwardButton.addEventListener('click', () => {skip(1)});
+progressBar.addEventListener('change',(e) => {
+    video.currentTime = e.target.value;
+})
