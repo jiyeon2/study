c87b43a7befa6b44cd5d3ff70ab5a6ad47c5c825
jiyeon lee <a1919361@gmail.com>
2020-07-31 14:04:53 +1200

    change script file

---
+++ scappy/script_1.js
@@ -0,0 +1,45 @@
+//addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
+(function(win, doc){
+	if(win.addEventListener)return;		//No need to polyfill
+
+	function docHijack(p){var old = doc[p];doc[p] = function(v){return addListen(old(v))}}
+	function addEvent(on, fn, self){
+		return (self = this).attachEvent('on' + on, function(e){
+			var e = e || win.event;
+			e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+			e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+			fn.call(self, e);
+		});
+	}
+	function addListen(obj, i){
+		if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+		else obj.addEventListener = addEvent;
+		return obj;
+	}
+
+	addListen([doc, win]);
+	if('Element' in win)win.Element.prototype.addEventListener = addEvent;			//IE8
+	else{		//IE < 8
+		doc.attachEvent('onreadystatechange', function(){addListen(doc.all)});		//Make sure we also init at domReady
+		docHijack('getElementsByTagName');
+		docHijack('getElementById');
+		docHijack('createElement');
+		addListen(doc.all);	
+	}
+})(window, document);
+
+var scappy = document.querySelector('.scappy');
+
+function addScappy(e){
+    
+    var newScappy = scappy.cloneNode(true);
+    newScappy.style.position='absolute';
+    newScappy.style.left = e.x-82 +'px';
+    newScappy.style.top = e.y-125+'px';
+    if (Math.random() > 0.7){
+        newScappy.classList.add('big-head');
+    }
+    document.body.appendChild(newScappy);
+
+}
+document.addEventListener('click', addScappy);
