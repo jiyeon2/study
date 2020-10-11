833cce33b76e77e879f56a44bb60f126d68f87d9
jiyeon lee <a1919361@gmail.com>
2020-10-11 22:12:29 +0900

    change confirm message

--- building/script.js
+++ building/script.js
@@ -1,27 +1,27 @@
-const house = document.querySelector('.building');
+const house = document.querySelector(".building");
 
-house.addEventListener('click', hide);
-house.addEventListener('hide', function(e) {
-    console.log(e);
-    if (confirm('Would you like to provoke preventDefault?')){
-        e.preventDefault();
-    }
+house.addEventListener("click", hide);
+house.addEventListener("hide", function (e) {
+  console.log(e);
+  if (confirm("Do you like this house?")) {
+    e.preventDefault();
+  }
 });
 
 function hide() {
-    let event = new CustomEvent('hide',{
-        cancelable: true,
-        detail:{
-            description: 'hide house'
-        }
-    });
+  let event = new CustomEvent("hide", {
+    cancelable: true,
+    detail: {
+      description: "hide house",
+    },
+  });
 
-    const returnFromDispatchEvent = house.dispatchEvent(event) 
-    console.log(returnFromDispatchEvent);
-    if (!returnFromDispatchEvent){
-        alert('canceled hiding by handler');
-    } else{
-        house.hidden = true;
-    }
+  const returnFromDispatchEvent = house.dispatchEvent(event);
+  console.log(returnFromDispatchEvent);
+  if (!returnFromDispatchEvent) {
+    alert("It won't be disappear if you like it.");
+  } else {
+    house.hidden = true;
+    document.querySelector("h1").textContent = "house removed";
+  }
 }
-
